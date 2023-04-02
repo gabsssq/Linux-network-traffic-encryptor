23f2e22d9d44ec9d6ff807b9f01eb903d7f31b8f
Petr Tůma <85123006+gabsssq@users.noreply.github.com>
2023-04-02 21:16:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,14 +26,14 @@ git clone https://github.com/itzmeanjan/kyber.git
 # Knihovny pro AES
 wget https://www.cryptopp.com/cryptopp870.zip
 unzip -aoq cryptopp870.zip -d cryptopp
-(cd cryptopp && make)
-(cd cryptopp && make install)
+(cd cryptopp && sudo make)
+(cd cryptopp && sudo make install)
 
 sudo ip tuntap add name tun0 mode tun
 sudo ip link set tun0 up
 sudo ip addr add 192.168.1.1 peer 192.168.1.2 dev tun0
 
 echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
-ip route add $Route_IP via 192.168.1.2
+sudo ip route add $Route_IP via 192.168.1.2
 
 g++ -std=c++20 -I /usr/local/include/ -I ./kyber/include/ -I ./kyber/subtle/include/ -I ./kyber/sha3/include/ ./sifrator/sifrator.cpp  /usr/local/lib/libcryptopp.a -o sifrator.exe
