0c5cf196343e4383071adfb58d958c8b2d7f8e5f
Petr Tůma <85123006+gabsssq@users.noreply.github.com>
2023-04-02 21:05:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ Help()
    echo
 }
 
-if [ $1 = "" || $1 = "-help" || $1 = "-h"]
+if [ $1 = "" ] || [ $1 = "-help" ] || [ $1 = "-h" ]
 then
 Help
 exit 1
@@ -29,8 +29,6 @@ unzip -aoq cryptopp870.zip -d cryptopp
 (cd cryptopp && make)
 (cd cryptopp && make install)
 
-git clone https://github.com/gabsssq/sifrator.git
-
 sudo ip tuntap add name tun0 mode tun
 sudo ip link set tun0 up
 sudo ip addr add 192.168.1.1 peer 192.168.1.2 dev tun0
@@ -38,4 +36,4 @@ sudo ip addr add 192.168.1.1 peer 192.168.1.2 dev tun0
 echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
 ip route add $Route_IP via 192.168.1.2
 
-g++ -std=c++20 -I /usr/local/include/ -I ../kyber/include/ -I ../kyber/subtle/include/ -I ../kyber/sha3/include/ ../sifrator/sifrator.cpp  /usr/local/lib/libcryptopp.a -o sifrator.exe
+g++ -std=c++20 -I /usr/local/include/ -I ./kyber/include/ -I ./kyber/subtle/include/ -I ./kyber/sha3/include/ ./sifrator/sifrator.cpp  /usr/local/lib/libcryptopp.a -o sifrator.exe
