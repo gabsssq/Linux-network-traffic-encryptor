61eb7cac33e0fa2fc9b749ad852eac01d2b2866c
Petr Tůma <85123006+gabsssq@users.noreply.github.com>
2023-04-02 19:57:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 Help()
 {
    echo "Pouziti: ./install.sh [Sit_druhe_brany]"
+   echo "Tvar site: x.x.x.x/y"
    echo
 }
 
@@ -15,8 +16,6 @@ fi
 # Udaj pro smerovani
 Route_IP=$1
 
-
-
 # Knihovny pro kyber
 git clone https://github.com/itzmeanjan/kyber.git
 (cd kyber && git submodule update --init)
@@ -27,5 +26,13 @@ unzip -aoq cryptopp870.zip -d cryptopp
 (cd cryptopp && make)
 (cd cryptopp && make install)
 
-g++ -std=c++20 -I /usr/local/include/ -I ./kyber/include/ -I ./kyber/subtle/include/ -I ./kyber/sha3/include/ sifrator.cpp  /usr/local/lib/libcryptopp.a -o sifrator.exe
+git clone https://github.com/gabsssq/sifrator.git
 
+sudo ip tuntap add name tun0 mode tun
+sudo ip link set tun0 up
+sudo ip addr add 192.168.1.1 peer 192.168.1.2 dev tun0
+
+echo "1" > /proc/sys/net/ipv4/ip_forward
+ip route add $Route_IP via 192.168.1.2
+
+g++ -std=c++20 -I /usr/local/include/ -I ../kyber/include/ -I ../kyber/subtle/include/ -I ../kyber/sha3/include/ ../sifrator/sifrator.cpp  /usr/local/lib/libcryptopp.a -o sifrator.exe
