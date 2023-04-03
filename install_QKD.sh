7a7a82da482e965d05f38ac4040a22c45e5a77f6
Petr Tůma <85123006+gabsssq@users.noreply.github.com>
2023-04-03 14:29:48 +0200

    Create install_QKD.sh

---
+++ install_QKD.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+apt-get install apache2
+apt-get install php7.4
+a2enmod php7.4
+service apache2 reload
+mkdir /var/www/html/klic
+mkdir /var/www/html/ID
+cp index-klic.php /var/www/html/klic/index.php
+cp index-ID.php /var/www/html/ID/index.php
