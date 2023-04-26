edb2acff7cc3e0cb6b6d88957a6f7c5c6c84a6e3
Petr Tůma <85123006+gabsssq@users.noreply.github.com>
2023-04-26 14:09:31 +0200

    Update ipencrypt.py

--- Python PoC/NFQUEUE/ipencrypt.py
+++ Python PoC/NFQUEUE/ipencrypt.py
@@ -4,17 +4,19 @@ from scapy.all import *
 from Cryptodome.Cipher import AES
 
     # Šifrovací funkce
-def encrypt(plaintext, key, mode):
+def encrypt(plaintext, key, mode, asociated_data):
 	encobj = AES.new(key, AES.MODE_GCM)
+	encobj.update(asociated_data)
 	ciphertext,authTag=encobj.encrypt_and_digest(plaintext)
 	return(ciphertext,authTag, encobj.nonce)
-
 
     # Šifrování paketu a nahrazení čísla protokolu v IP hlavičce za vlastní
 def listener(packet):
 	scapy_packet = IP(packet.get_payload())
 	ip_len = scapy_packet[IP].ihl * 4
-	ciphertext = encrypt(packet.get_payload()[9].to_bytes(1,"big")+packet.get_payload()[ip_len:], key, AES.MODE_GCM)
+	# zabezpeci celou hlavicku krome checksum a ttl proti zmene utocnikem
+	# pouzije se protokol 150 a delka se zvetsi o 33
+	ciphertext = encrypt(packet.get_payload()[9].to_bytes(1,"big")+packet.get_payload()[ip_len:], key, AES.MODE_GCM, packet.get_payload()[:2]+(int.from_bytes(packet.get_payload()[2:4],"big")+33).to_bytes(2,"big")+packet.get_payload()[4:8]+(150).to_bytes(1,"big")+packet.get_payload()[12:ip_len])
 	res_packet = IP(packet.get_payload()[:ip_len])
 	res_packet = res_packet/(ciphertext[2]+ciphertext[1]+ciphertext[0])
 	res_packet[IP].proto = "mujPr"
