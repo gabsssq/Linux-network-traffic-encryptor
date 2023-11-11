4c44cf2c14e827f6173374a64262c89704746561
Jan Havlin <241030@vutbr.cz>
2023-11-11 12:40:16 +0100

    test

--- encryptor_client.cpp
+++ encryptor_client.cpp
@@ -595,8 +595,10 @@ SecByteBlock rekey_cli(int client_fd, string qkd_ip, const char *srv_ip)
     string salt = time + std::to_string(counter);
 
     string pqc_key = get_pqckey(client_fd);
+    cout << "PQC key: " << pqc_key << endl;
 
-    string ecdh_key = PerformECDHKeyExchange(client_fd, srv_ip);
+    string ecdh_key = PerformECDHKeyExchange(&client_fd, srv_ip);
+    cout << "ECDH key: " << ecdh_key << endl;
 
     if (qkd_ip.empty())
     {
