97b3b92bbd5f1ac148dfe8a2a226866c8127e778
Jan Havlin <241030@vutbr.cz>
2023-11-14 07:04:39 +0100

    test

--- encryptor_server.cpp
+++ encryptor_server.cpp
@@ -714,6 +714,7 @@ SecByteBlock rekey_srv(int new_socket, string qkd_ip, char bufferTCP[MAXLINE])
         std::stringstream buffer;
         buffer << t.rdbuf();
         string buffer_str = buffer.str();
+        cout << "QKD key: " << buffer_str << endl;
 
         // all parameters set, starting to creating hybrid key
         string key_one = hmac_hashing(salt, pqc_key);
