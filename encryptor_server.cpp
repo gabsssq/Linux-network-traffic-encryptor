35425d69fd1d7277c85b79904cb3ac122b709bb6
Jan Havlin <241030@vutbr.cz>
2023-11-14 07:19:27 +0100

    test

--- encryptor_server.cpp
+++ encryptor_server.cpp
@@ -453,13 +453,13 @@ void get_qkdkey(string qkd_ip, char bufferTCP[MAXLINE])
     myfile.open("keyID");
     myfile << bufferTCP;
     myfile.close();
-    // convert bufferTCP to string
-    std::stringstream bufferTCP_string;
-    bufferTCP_string << bufferTCP;
 
     // Obtain QKD key with keyID
     system(("./sym-ExpQKD 'server' " + qkd_ip).c_str());
 
+    // convert bufferTCP to string
+    std::stringstream bufferTCP_string;
+    bufferTCP_string << bufferTCP;
     // hash content of bufferTCP with SHAKE128
     shake128_hash.Update((const byte *)bufferTCP_string.str().c_str(), bufferTCP_string.str().length());
     string pom_param;
